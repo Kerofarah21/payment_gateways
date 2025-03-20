8dbe76f0755701e551c1b8bb3689569a7696afb8
Kerolos Farah <k.farah2124@gmail.com>
2025-03-20 19:27:17 +0200

    create bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'Features/checkout/presentation/views/my_cart_view.dart';
 import 'core/utils/dio_helper.dart';
+import 'core/utils/my_bloc_observer.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+
   DioHelper.init();
+
+  Bloc.observer = MyBlocObserver();
 
   runApp(
     const PaymentGatewaysApp(),
